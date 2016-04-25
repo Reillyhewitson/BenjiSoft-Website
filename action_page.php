b08c96bee550d51ccb1651c0faef4f74426ee622
Benji <benji@benjicraft.info>
2016-04-25 03:13:45 -0400

    Applied fixes from StyleCI

--- action_page.php
+++ action_page.php
@@ -1,26 +1,25 @@
 <?php
-$con = mysql_connect("dbaddress","username","password");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
-mysql_select_db("customform", $con);
-$sql="INSERT INTO formdata (Fname, Lname, Email, Message)
+
+$con = mysql_connect('dbaddress', 'username', 'password');
+if (!$con) {
+    die('Could not connect: '.mysql_error());
+}
+mysql_select_db('customform', $con);
+$sql = "INSERT INTO formdata (Fname, Lname, Email, Message)
 VALUES ('$_POST[Fname]','$_POST[Lname]','$_POST[Email]','$_POST[Message]')";
-if (!mysql_query($sql,$con))
-  {
-  die('Error: ' . mysql_error());
-  }
-echo "Your Information Was Successfully Posted";
+if (!mysql_query($sql, $con)) {
+    die('Error: '.mysql_error());
+}
+echo 'Your Information Was Successfully Posted';
 mysql_close($con);
-$to = "benji@benjicraft.info";
-$subject = "Question from website";
-$email = $_POST['Email'] ;
-$message = $_POST['Comp'] ;
+$to = 'benji@benjicraft.info';
+$subject = 'Question from website';
+$email = $_POST['Email'];
+$message = $_POST['Comp'];
 $headers = "From: $Email";
-$sent = mail($to, $subject, $message, $headers) ;
-if($sent)
-{print "Your message was sent successfully"; }
-else
-{print "We encountered an error sending your message"; }
-?>
+$sent = mail($to, $subject, $message, $headers);
+if ($sent) {
+    echo 'Your message was sent successfully';
+} else {
+    echo 'We encountered an error sending your message';
+}
