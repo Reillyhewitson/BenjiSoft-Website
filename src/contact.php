ca76cce4f70ae4f829e61ac7c603fa8b1bedc5df
Benji <benji@benjicraft.info>
2018-05-23 20:23:43 +0100

    Added blank pages

---
+++ src/contact.php
@@ -0,0 +1,15 @@
+<!-- Made with Love by BenjiSoft
+     Licensed under the GNU GPL v3.0 license -->
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BenjaminLewis
+ * Date: 23/05/2018
+ * Time: 19:55
+ */
+$content = "
+<h1>Welcome!</h1>
+<h2>We're currently doing some work on the site</h2>
+<h3>And apologise if we don't look our best</h3>
+";
+include_once 'lib/php/template.php';
