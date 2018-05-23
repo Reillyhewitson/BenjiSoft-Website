d8e066408cea042a175d0f37f979fce7865a9413
Benji <benji@benjicraft.info>
2018-05-23 20:04:52 +0100

    Dankified the template
    Now the template is only 3 lines

--- src/index.php
+++ src/index.php
@@ -1,31 +1,15 @@
 <!-- Made with Love by BenjiSoft
      Licensed under the GNU GPL v3.0 license -->
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>BenjiSoft</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="lib/css/main.css" />
-</head>
-<body>
-    <div id="header">
-        <img src="lib/res/logo.gif" height="200%">
-        <div class="right">
-            <a href="index.php"
-                <?php
-                    if (basename($_SERVER['PHP_SELF']) == 'index.php') {
-                        echo 'class="nav-current"';
-                    } else {
-                        echo 'class="nav"';
-                    }
-                    ?>>Home</a>
-        </div>
-    </div>
-    <div id="content">
-
-    </div>
-    <!-- <?php include 'lib/includes/header.php'?> -->
-</body>
-</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BenjaminLewis
+ * Date: 23/05/2018
+ * Time: 19:55
+ */
+$content = "
+<h1> Who are we?</h1>
+<p>Well good question!</p>
+<p>Let's explain!</p>
+";
+include_once 'lib/php/template.php';
---
+++ src/lib/php/template.php
@@ -0,0 +1,18 @@
+<!-- Made with Love by BenjiSoft
+     Licensed under the GNU GPL v3.0 license -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>BenjiSoft</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="lib/css/main.css" />
+</head>
+<body>
+    <?php include 'lib/php/header.php'?>
+    <div id="content">
+        <?php echo $content;?>
+    </div>
+</body>
+</html>
