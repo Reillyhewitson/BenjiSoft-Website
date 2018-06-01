316bcbf4abe7984a5781c8ab982da8d080ac7b6d
Benji <benji@benjicraft.info>
2018-06-01 21:45:07 +0100

    Fixed typo

--- src/about.php
+++ src/about.php
@@ -9,6 +9,6 @@
  */
 $content = "
 <h1>Who are we?</h1>
-<p>We are a team of 3 who are providing heavilly subsided web design and management services to small businesses and individuals who need some help getting started. Our plans start from as low as £5 per month which includes all of the management and hosting of your website. All you need to do is specify what you want from your site and we will make it happen!</p>
+<p>We are a team of 3 who are providing heavily subsidised web design and management services to small businesses and individuals who need some help getting started. Our plans start from as low as £5 per month which includes all of the management and hosting of your website. All you need to do is specify what you want from your site and we will make it happen!</p>
 ";
 include_once 'lib/php/template.php';
