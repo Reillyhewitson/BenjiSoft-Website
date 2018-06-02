83bfb56ec20b947ad030438cf419abb19469ff7b
Benji <benji@benjicraft.info>
2018-06-02 10:33:27 +0100

    WIP Contact Page

--- src/contact.php
+++ src/contact.php
@@ -8,8 +8,20 @@
  * Time: 19:55.
  */
 $content = "
-<h1>Welcome!</h1>
-<h2>We're currently doing some work on the site</h2>
-<h3>And apologise if we don't look our best</h3>
+<link rel='stylesheet' type='text/css' href='lib/css/contact.css'>
+<h3 class='center'>If you have a general enquiree please email us at <a href='mailto:contact@benjisoft.org.uk'>contact@benjisoft.org.uk</a>. If you would like to get in touch with someone specifically, please use their contact details below.</h3>
+<br>
+<table class='center'>
+<tr>
+<td>Oliver Beardsall</td>
+<td>Benjamin Lewis</td>
+<td>Reilly Hewitson</td>
+</tr>
+<tr>
+<td>oliver@benjisoft.org.uk</td>
+<td>benji@benjisoft.org.uk</td>
+<td>reilly@benjisoft.org.uk</td>
+</tr>
+</table>
 ";
 include_once 'lib/php/template.php';
