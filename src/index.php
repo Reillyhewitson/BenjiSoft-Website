630e4574c25684ec2512b0624391a5f13ed25638
Benji <benji@benjicraft.info>
2018-06-02 10:52:40 +0100

    Finished home page

--- src/index.php
+++ src/index.php
@@ -8,8 +8,12 @@
  * Time: 19:55.
  */
 $content = "
-<h1>Welcome!</h1>
+<h1>Welcome to BenjiSoft!</h1>
 <h2>We're currently doing some work on the site</h2>
 <h3>And apologise if we don't look our best</h3>
+<h4>But in the mean time...</h4>
+<p>Are you a small business? Are you in need of a website but hate the rediculous amount that most businesses charge for them. Do you need something that's just simple?</p>
+<p>If you answered yes to any of the above questions, then well done! You are in the right place. We help out small businesses by providing them with close to free web hosting, management & design solutions. This all starts from as low as £5 per month! </p>
+<p><a role='button' class='btn btn-info' href='contact.php'>Get in touch today!</a></p>
 ";
 include_once 'lib/php/template.php';
