121a8203aac4998087ddbd8f45bdba7da853fb9c
Unknown <reilly.hewitson@utcreading.org.uk>
2018-06-08 13:39:58 +0100

    Adjusted navbar design
    Now aligns with logo and has an effect on hover.

--- src/lib/php/header.php
+++ src/lib/php/header.php
@@ -1,47 +1,47 @@
 <div id="header">
-    <a href = "index.php" id = "logo"><img src="lib/res/logo.png" height="110" id = "logo"></a>
-    <div id="links" class="righta">
-        <a href="index.php"
-            <?php
-            if (basename($_SERVER['PHP_SELF']) == 'index.php') {
-                echo 'class="nav-current"';
-            } else {
-                echo 'class="nav"';
-            }
-            ?>>Home</a> &nbsp;
-        <a href="about.php"
-            <?php
-            if (basename($_SERVER['PHP_SELF']) == 'about.php') {
-                echo 'class="nav-current"';
-            } else {
-                echo 'class="nav"';
-            }
-            ?>>About</a> &nbsp;
-        <a href="projects.php"
-            <?php
-            if (basename($_SERVER['PHP_SELF']) == 'projects.php') {
-                echo 'class="nav-current"';
-            } else {
-                echo 'class="nav"';
-            }
-            ?>>Projects</a> &nbsp;
-        <a href="team.php"
-            <?php
-            if (basename($_SERVER['PHP_SELF']) == 'team.php') {
-                echo 'class="nav-current"';
-            } else {
-                echo 'class="nav"';
-            }
-            ?>>The Team</a> &nbsp;
-        <a href="contact.php"
-            <?php
-            if (basename($_SERVER['PHP_SELF']) == 'contact.php') {
-                echo 'class="nav-current"';
-            } else {
-                echo 'class="nav"';
-            }
-            ?>>Contact Us</a> &nbsp;
-    </div>
+        <a href = "index.php" id = "logo"><img src="lib/res/logo.png" height="110" id = "logo"></a>  
+        <div id="links" class="righta">
+            <a href="index.php"
+                <?php
+                if (basename($_SERVER['PHP_SELF']) == 'index.php') {
+                    echo 'class="nav-current"';
+                } else {
+                    echo 'class="nav"';
+                }
+                ?>>Home</a> &nbsp;
+            <a href="about.php"
+                <?php
+                if (basename($_SERVER['PHP_SELF']) == 'about.php') {
+                    echo 'class="nav-current"';
+                } else {
+                    echo 'class="nav"';
+                }
+                ?>>About</a> &nbsp;
+            <a href="projects.php"
+                <?php
+                if (basename($_SERVER['PHP_SELF']) == 'projects.php') {
+                    echo 'class="nav-current"';
+                } else {
+                    echo 'class="nav"';
+                }
+                ?>>Projects</a> &nbsp;
+            <a href="team.php"
+                <?php
+                if (basename($_SERVER['PHP_SELF']) == 'team.php') {
+                    echo 'class="nav-current"';
+                } else {
+                    echo 'class="nav"';
+                }
+                ?>>The Team</a> &nbsp;
+            <a href="contact.php"
+                <?php
+                if (basename($_SERVER['PHP_SELF']) == 'contact.php') {
+                    echo 'class="nav-current"';
+                } else {
+                    echo 'class="nav"';
+                }
+                ?>>Contact Us</a> &nbsp;
+        </div>
     <div>
     <select id = "dropdown" onchange = "location = this.value">
         <option value="" selected = "selected">Select</option>
