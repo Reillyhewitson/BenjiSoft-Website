ecf32642f1e84906e94d9e00d5d34d00aacca163
Unknown <reilly.hewitson@utcreading.org.uk>
2018-06-19 15:55:25 +0100

    Translation

--- src/contact.php
+++ src/contact.php
@@ -9,6 +9,7 @@
  */
 $content = "
 <link rel='stylesheet' type='text/css' href='lib/css/contact.css'>
+<iframe src='https://docs.google.com/forms/d/e/1FAIpQLSej-00SwYSWGX_uMv4xi5jN2By4jsVBxFAEbYTSJJtM4yFGaw/viewform?embedded=true' width='100%' height='520px' frameborder='0' marginheight='0' marginwidth='0'>Loading...</iframe>
 <h3 class='center'>If you have a general enquiry please email us at <a href='mailto:contact@benjisoft.org.uk'>contact@benjisoft.org.uk</a>. If you would like to get in touch with someone specifically, please use their contact details below.</h3>
 <br>
 <table class='contact_table'>
